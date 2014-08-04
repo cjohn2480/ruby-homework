b7df26f47752d38febcc7147acd4bf07ab0baa6f
Chioke A. Johnson <cjohn30001@yahoo.com>
2014-08-03 23:49:33 -0400

    updated bottles homework

--- 99bottles3.rb
+++ 99bottles3.rb
@@ -1,3 +1,10 @@
+# Outline
+# Create variable of 99 for bottles of beer
+# While that number is above 1, print that number and bottles of beer on the wall
+# Also while that number is above 1, print Take one down, pass it around line
+# When variable is at 1, change plural bottles to singular bottle
+# Also while variable is at 1, Take one down line ends with no more bottles of beer on the wall
+
 var1 = 99
 while var1 > 1
 	puts "#{var1} bottles of beer on the wall, #{var1} bottles of beer."
